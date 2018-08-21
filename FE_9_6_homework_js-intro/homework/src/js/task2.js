da88f0abfa3cd63dc950b57feef0bcbebf7e15ce
Alex <syvoplias76@gmail.com>
2018-08-21 22:12:20 +0300

    done

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,37 +1,9 @@
-const sideA = parseFloat(prompt('Enter A side: ', '0'));
-const sideB = parseFloat(prompt('Enter B side: ', '0'));
-const angle = parseFloat(prompt('Enter angle: ', '0'));
+const firstSide = parseFloat(prompt('Enter length of first side:', 0));
+const secondSide = parseFloat(prompt('Enter length of second side:', 0));
+const angle = parseFloat(prompt('Enter angle:', 0));
 
-const ANGLE_SUM = 180;
-
-const outputTemplate = (sideC, square, perimeter) => `
-C side length: ${+sideC.toFixed(2)} 
-Triangle square: ${+square.toFixed(2)}
-Triangle perimeter: ${+perimeter.toFixed(2)}
-`;
-
-let output;
-
-if (validateInput(sideA) || validateInput(sideB) || validateInput(angle) || angle > ANGLE_SUM) {
-    output = 'Invalid data';
-} else {
-    const sideC = getSideC(sideA, sideB, angle);
-    const perimeter = sideA + sideB + sideC;
-    const square = getSquare(perimeter / 2, sideA, sideB, sideC);
-    output = outputTemplate(sideC, square, perimeter);
+let thirdSide = function (firstSide, secondSide, angle) {
+    return Math.SQRT2(firstSide ** 2 + secondSide ** 2 - 2 * firstSide * secondSide * Math.cos(angle));
 }
 
-function validateInput(number) {
-    return isNaN(number) || typeof number !== 'number' || number < 0;
-}
-
-function getSideC(sideA, sideB, angle) {
-    const gamma = Math.PI / ANGLE_SUM * angle;
-    return Math.sqrt(sideA * sideA + sideB * sideB - 2 * sideA * sideB * Math.cos(gamma));
-}
-
-function getSquare(perimeter, sideA, sideB, sideC) {
-    return Math.sqrt(perimeter * ((perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC)));
-}
-
-console.log(output);
+console.log(thirdSide(firstSide, secondSide, angle));
--- FE_9_7_homework_control-flow/homework/src/js/task1.js
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -1,1 +1,21 @@
-// Your code goes here
+let login = prompt('Enter your login, please: ', '');
+if (login === '' || login === null) {
+    alert('Canceled.');
+} else if (login.length < 4) {
+    alert('I don\'t know any users having name length less than 4 symbols.')
+} else if (login === 'User') {
+    let password = prompt('Enter your password, please: ', '');
+    if (password === '' || password === null) {
+        alert('Canceled.');
+    } else if (password === 'SuperUser') {
+        if (new Date().getHours() < 20) {
+            alert('Good day!')
+        } else {
+            alert('Good Evening!');
+        }
+    } else {
+        alert('Wrong password');
+    }
+} else {
+    alert('I don\'t know you.');
+}
