ca6dbc8848958631b346a9e7dce2ef96ba3a8874
Alex <syvoplias76@gmail.com>
2018-09-10 02:22:25 +0300

    some changes

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,116 +1,142 @@
 let rootNode = document.getElementById('root');
+let contentNode = (function () {
 
-let contentNode = (function createList() {
-    let content = document.createElement('section');
+    let content = document.createElement('div');
     content.setAttribute('class', 'main-container');
 
-    let headerElements = document.createElement('div');
-    headerElements.setAttribute('class', 'headerElements');
-
-    let heading = document.createElement('h1');
-    heading.innerHTML = 'TODO Cat List';
-    headerElements.appendChild(heading);
-
-    let inputBlock = document.createElement('div');
-    inputBlock.setAttribute('class', 'header-input-block');
-
-    let input = document.createElement('input');
-    input.setAttribute('id', 'headerInputField');
-    input.setAttribute('placeholder', 'Add New Action');
-
-    inputBlock.appendChild(input);
-
-    let addButton = document.createElement('i');
-    addButton.setAttribute('class', 'material-icons');
-    addButton.innerHTML = 'add_box';
-    addButton.addEventListener('click', addListItem);
-    inputBlock.appendChild(addButton);
-
-    headerElements.appendChild(inputBlock);
-    content.appendChild(headerElements);
-
-    let listItemsBlock = document.createElement('div');
-    listItemsBlock.setAttribute('class', 'listItemsBlock');
-    let list = document.createElement('ul');
-    listItemsBlock.appendChild(list);
-    content.appendChild(listItemsBlock);
-
-    let footerElements = document.createElement('div');
-    footerElements.setAttribute('class', 'footerElements');
-    let catImg = document.createElement('img');
-    catImg.setAttribute('src', './assets/img/cat.png');
-    footerElements.appendChild(catImg);
-
-    content.appendChild(footerElements);
-
-    let data = {
-        allItems: []
+    const page = {
+        headerElements: document.createElement('div'),
+        listItemsBlock: document.createElement('div'),
+        footerElements: document.createElement('div')
     }
 
-    class Item {
-        constructor(id) {
-            this.id = id;
-        }
+    const headerElements = {
+        heading: document.createElement('h1'),
+        inputBlock: document.createElement('div'),
+        input: document.createElement('input'),
+        addButton: document.createElement('button'),
+        addBtnIco: document.createElement('i'),
+        message: 'Maximum item per list are created'
     }
 
-    function addListItem() {
-        const ZERO = 0;
-        const ONE = 1;
-        const listMaxItems = 10;
-        let inputValue = document.getElementById('headerInputField').value;
-        let message = document.createElement('p');
-        message.setAttribute('class', 'message');
-        if (data.allItems.length === listMaxItems) {
+    const listBlock = {
+        list: document.createElement('ul')
+    }
 
-            addButton.style.display = 'none';
-            if (document.getElementsByClassName('message').length === ZERO) {
-                message.innerHTML = 'Maximum item per list are created';
-                headerElements.appendChild(message);
-            }
+    const footerElements = {
+        catImg: document.createElement('img')
+    }
 
+    // Attributes for header elements
+    page.headerElements.setAttribute('class', 'header-elements');
+    headerElements.heading.innerHTML = 'TODO Cat List';
+    page.headerElements.appendChild(headerElements.heading);
+    headerElements.inputBlock.setAttribute('class', 'header-input-block');
+    headerElements.input.setAttribute('id', 'headerInputField');
+    headerElements.input.autofocus = true;
+    headerElements.input.setAttribute('placeholder', 'Add New Action');
+    headerElements.addButton.appendChild(headerElements.addBtnIco);
+    headerElements.addButton.disabled = true;
+    headerElements.inputBlock.appendChild(headerElements.input);
+    headerElements.addBtnIco.setAttribute('class', 'material-icons');
+    headerElements.addBtnIco.innerHTML = 'add_box';
+    headerElements.addButton.addEventListener('click', addListItem);
+    headerElements.inputBlock.appendChild(headerElements.addButton);
+    page.headerElements.appendChild(headerElements.inputBlock);
+    content.appendChild(page.headerElements);
+
+    // Attributes for list block elements 
+    listBlock.list.setAttribute('id', 'items');
+    page.listItemsBlock.setAttribute('class', 'list-items-block');
+    page.listItemsBlock.appendChild(listBlock.list);
+    content.appendChild(page.listItemsBlock);
+
+    // Attributes for footer elements
+    page.footerElements.setAttribute('class', 'footer-elements');
+    footerElements.catImg.setAttribute('src', './assets/img/cat.png');
+    page.footerElements.appendChild(footerElements.catImg);
+    content.appendChild(page.footerElements);
+    let items = {
+        allItems: [],
+        ZERO: 0,
+        ONE: 1,
+        maxListItems: 10
+    }
+
+    headerElements.input.addEventListener('keyup', () => {
+        if (headerElements.input.value.length === 0) {
+            headerElements.addButton.disabled = true;
         } else {
-            if (inputValue !== '') {
+            headerElements.addButton.disabled = false;
+        }
+    })
+
+    // Dragging
+
+
+    // Creating list item
+    function addListItem() {
+        let listComp = {
+            inputField: document.getElementById('headerInputField'),
+            inputValue: document.getElementById('headerInputField').value,
+            item: document.createElement('li'),
+            itemsFirstBlock: document.createElement('div'),
+            ico: document.createElement('i'),
+            deleteIco: document.createElement('i'),
+            description: document.createElement('p')
+        }
+        if (items.allItems.length < items.maxListItems) {
+            if (listComp.inputValue > '') {
                 let newItem, ID;
-                if (data.allItems.length > ZERO) {
-                    ID = data.allItems[data.allItems.length - ONE].id + ONE;
-                } else {
-                    ID = ZERO;
+                class Item {
+                    constructor(ID, desc) {
+                        this.ID = ID;
+                        this.desc = desc;
+                    }
                 }
-                newItem = new Item(ID);
-                data.allItems.push(newItem);
+                items.allItems.length > items.ZERO ?
+                    ID = items.allItems[items.allItems.length - items.ONE].ID + items.ONE :
+                    ID = items.ZERO;
+                newItem = new Item(ID, listComp.inputValue.trim());
+                items.allItems.push(newItem);
 
-                const item = document.createElement('li');
-                item.setAttribute('class', `items item-${ID}`);
-
-                const itemsFirstBlock = document.createElement('div');
-                const ico = document.createElement('i');
-                const deleteIco = document.createElement('i');
-                deleteIco.setAttribute('class', 'material-icons');
-                deleteIco.innerHTML = 'delete';
-                deleteIco.addEventListener('click', () => {
-                    item.remove();
-                    message.remove();
-                    data.allItems.pop();
-                    addButton.style.display = 'inline-block';
-                    headerElements.removeChild(message);
+                //Attributes for item elements
+                listComp.item.setAttribute('class', `item item-${ID}`);
+                listComp.item.setAttribute('draggable', 'true');
+                listComp.deleteIco.setAttribute('class', 'material-icons');
+                listComp.deleteIco.innerHTML = 'delete';
+                listComp.deleteIco.addEventListener('click', () => {
+                    if (document.querySelector('.message')) {
+                        document.querySelector('.message').remove();
+                    }
+                    items.allItems.pop();
+                    listComp.item.remove();
+                    headerElements.addButton.disabled = false;
                 });
-
-                ico.setAttribute('class', 'material-icons');
-                ico.innerHTML = 'check_box_outline_blank';
-                ico.addEventListener('click', () => {
-                    ico.innerHTML = 'check_box';
+                listComp.ico.setAttribute('class', 'material-icons');
+                listComp.ico.innerHTML = 'check_box_outline_blank';
+                listComp.ico.addEventListener('click', () => {
+                    listComp.ico.innerHTML = 'check_box';
                 });
-                itemsFirstBlock.appendChild(ico);
-                let description = document.createElement('p');
-                description.innerHTML = inputValue;
-                itemsFirstBlock.appendChild(description);
-                item.appendChild(itemsFirstBlock);
-                item.appendChild(deleteIco);
-                list.appendChild(item);
+                listComp.itemsFirstBlock.appendChild(listComp.ico);
+                listComp.description.innerHTML = listComp.inputValue;
+                listComp.itemsFirstBlock.appendChild(listComp.description);
+                listComp.item.appendChild(listComp.itemsFirstBlock);
+                listComp.item.appendChild(listComp.deleteIco);
+                listBlock.list.appendChild(listComp.item);
+
+                if (items.allItems.length >= items.maxListItems) {
+                    if (document.getElementsByClassName('message').length === items.ZERO) {
+                        let errorMessage = document.createElement('p');
+                        errorMessage.setAttribute('class', 'message');
+                        errorMessage.innerHTML = headerElements.message;
+                        page.headerElements.appendChild(errorMessage);
+                        headerElements.addButton.disabled = true;
+                    }
+                }
             }
         }
     }
-
     return content;
 })();
 
